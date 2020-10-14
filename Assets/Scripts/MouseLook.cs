807f1620907b9938be8ea4f617486ce5ef939520
GGfrozen <joen4ik@gmail.com>
2020-10-14 16:47:14 +0300

    Initial commit

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensivity = 1f;
+    [SerializeField] private Transform mainCamera;
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+
+    private float _xRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        var mouseHorizontal = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        var mouseVertical = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+        _xRotation -= mouseVertical;
+        _xRotation = Mathf.Clamp(_xRotation, minX, maxX);
+        mainCamera.localRotation = Quaternion.Euler(_xRotation,0,0);
+        transform.Rotate(Vector3.up,mouseHorizontal);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private Camera weaponCamera;
+    [SerializeField] private int normalView = 60;
+    [SerializeField] private int aimView = 30;
+    [SerializeField] private bool arrayController;
+    
+    private bool _isAim;
+
+    private void Start()
+    {
+   
+        _isAim = false;
+    }
+
+    private void Update()
+    {
+        UseAim();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        if (!arrayController)
+        {
+            MoveLogic("HorizontalWASD", "VerticalWASD");
+        }
+        else
+        {
+            MoveLogic("HorizontalArray", "VerticalArray");
+        }
+        
+    }
+
+    private void MoveLogic(string s1Horizontal, string s2Vertical)
+    {
+        var inputHorizontal = Input.GetAxis(s1Horizontal);
+        var inputVertical = Input.GetAxis(s2Vertical);
+        var movement = new Vector3(inputHorizontal, 0.0f, inputVertical);
+        transform.Translate(movement * (moveSpeed * Time.deltaTime));
+    }
+
+    private void UseAim()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (!_isAim)
+            {
+                playerCamera.fieldOfView = aimView;
+                weaponCamera.fieldOfView = aimView;
+                _isAim = true;
+            }
+            else
+            {
+                playerCamera.fieldOfView = normalView;
+                weaponCamera.fieldOfView = normalView;
+                _isAim = false;
+            }
+        }
+
+    }
+}
+
+    
