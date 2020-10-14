7f38e9bd7f49212e9add10649eebc1b553e97212
GGfrozen <joen4ik@gmail.com>
2020-10-14 18:08:03 +0300

    AddMirror

---
+++ Assets/Scripts/MirrortTexture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrortTexture : MonoBehaviour
+{
+    [SerializeField] private Camera mirrorCamera;
+
+    private MeshRenderer mesh;
+    private RenderTexture rt;
+
+    private void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        rt = new RenderTexture(256, 256, 24, RenderTextureFormat.ARGB32);
+        var mirrorMateril = mesh.material;
+        mirrorMateril.mainTexture = rt;
+        mirrorCamera.targetTexture = rt;
+    }
+}
